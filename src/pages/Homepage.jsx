0fe41c40077a981926f927cfa256a64aba933d09
codyivy0 <codyivy0@gmail.com>
2024-01-24 15:07:40 -0600

    homepage find section responsive

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,19 @@
+import styles from "./Homepage.module.css";
 const Homepage = () => {
   return (
-    <div>
-      This is home
-    </div>
-  )
+    <section className={styles.section_find}>
+      <div className={styles.findContent}>
+        <h1>
+          Find the best <span className={styles.accent}>talent</span>
+        </h1>
+        <p>
+          Finding the right people and building high performing teams can be hard.
+          Most companies arent tapping into the abundance of global talent. Were
+          about to change that.
+        </p>
+      </div>
+    </section>
+  );
 };
 
-export default Homepage ;
+export default Homepage;
