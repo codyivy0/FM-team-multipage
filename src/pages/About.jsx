f6594b15c4611b0da10c83fff3daf9a214e9d6c7
codyivy0 <codyivy0@gmail.com>
2024-01-25 14:32:08 -0600

    About page/ about section responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,19 @@
+import styles from "./About.module.css";
+
 const About = () => {
   return (
-    <div>
-      this is about
-    </div>
-  )
+    <>
+      <section className={styles.section_about}>
+        <h1>About</h1>
+        <p>
+          We help companies build dynamic teams made up of top global talent.
+          Using our network of passionate professionals we drive innovation and
+          deliver incredible outcomes. Talented, diverse teams shape the best
+          products and experiences. We&apos;ll bring those teams to you.
+        </p>
+      </section>
+    </>
+  );
 };
 
-export default About ;
+export default About;
